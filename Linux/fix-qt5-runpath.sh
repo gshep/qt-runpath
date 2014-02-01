32a04b3f13b17aca5646c5e8c1da99f127a3b7d7
Georgy Shepelev <geo.shepelev@gmail.com>
2014-02-01 23:07:35 +0400

    Finish script for Qt5 for Linux

--- Linux/fix-qt5-runpath.sh
+++ Linux/fix-qt5-runpath.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# firstly do it for the libraries itself
-find qtbase/src -name \*\.pro \! -path \*plugins\* | while read PROJECT_FILE;
+# firstly do it for the libraries itself except plugins and tools
+find qtbase/src -name \*\.pro \! -path \*src/plugins\* \! -path \*src/tools\* | while read PROJECT_FILE;
 do
 	grep --silent TARGET\[\[\:space\:\]\]\*\= $PROJECT_FILE
 	if [ $? -eq 0 ]; then
@@ -15,11 +15,15 @@ do
 	fi
 done
 
-# then do it for the rest modules except QtQuick1 as it has been
-# deprecated
-# also ignore qttools -
-find . -name \*\.pro \! -path \*qtbase\* \! -path \*qtquick1\* \! -path \*qttools\* | while read PROJECT_FILE;
+# then do it for the rest modules except qtbase
+# ignore plugins, tools and apps
+find . -name \*\.pro \! -path \*qtbase\* | while read PROJECT_FILE;
 do
+	grep -E --silent 'load\(qt_(plugin|tool|app)' $PROJECT_FILE
+	if [ $? -eq 0 ]; then
+		continue
+	fi
+
 	grep --silent TARGET\[\[\:space\:\]\]\*\= $PROJECT_FILE
 	if [ $? -eq 0 ]; then
 		echo $PROJECT_FILE
@@ -32,10 +36,10 @@ do
 	fi
 done
 
-# fix rpath in qttools
-find qttools/ -name \*\.pro | while read PROJECT_FILE;
+# fix rpath for not deployed tools and apps
+find . -name \*\.pro | while read PROJECT_FILE;
 do
-	grep --silent TARGET\[\[\:space\:\]\]\*\= $PROJECT_FILE
+	grep -E --silent 'load\(qt_(tool|app)' $PROJECT_FILE
 	if [ $? -eq 0 ]; then
 		echo $PROJECT_FILE
 		echo >> $PROJECT_FILE
@@ -49,9 +53,12 @@ do
 done
 
 # and at the end fix RPATH for the plugins
-find qtbase/src/plugins -name \*\.pro | while read PROJECT_FILE;
+# they should have two runpaths:
+#    1) for installed Qt
+#    2) for deployed Qt
+find . -name \*\.pro | while read PROJECT_FILE;
 do
-	grep --silent TARGET\[\[\:space\:\]\]\*\= $PROJECT_FILE
+	grep -E --silent 'load\(qt_plugin' $PROJECT_FILE
 	if [ $? -eq 0 ]; then
 		echo $PROJECT_FILE
 		echo >> $PROJECT_FILE
